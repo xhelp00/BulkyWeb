c78f6a952cb9b904af9c38f1dbd739c9f4bf7970
xhelp00 <xhelp00@gmail.com>
2024-07-02 13:59:33 +0200

    category controller

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace BulkyWeb.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		 
+		public IActionResult Index()
+		{
+			List<Category> objCategoryList = _db.Categories.ToList();
+			return View();
+		}
+	}
+}
